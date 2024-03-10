d598b3df4a5f1d85ef7d7c3f3de379d3fe3b5b51
BenJameto <martinez.benjaminosorio@gmail.com>
2024-03-09 22:35:00 -0600

    se agregaron clases de patron itarator

---
+++ Hamburguesa.java
@@ -0,0 +1,23 @@
+public class Hamburguesa {
+
+    String id;
+    String nombre;
+    String descripcion;
+    int precio;
+    boolean tieneQueso;
+    boolean esVegetariano;
+
+    public Hamburguesa(String id, String nombre, String descripcion, int precio, boolean tieneQueso, boolean esVegetariano ){
+        this.id = id;
+        this.nombre =nombre;
+        this.descripcion=descripcion;
+        this.precio =precio;
+        this.tieneQueso=tieneQueso;
+        this.esVegetariano=esVegetariano;
+    }
+    @Override
+    public String toString (){
+        String cadena = id +" "+ nombre+ " \n"+ descripcion + precio + tieneQueso +esVegetariano;
+        return cadena;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Lector lector = new Lector ();
+        Basico menu = new Basico();
+        lector.leeMenu(menu);
+    }
+}
