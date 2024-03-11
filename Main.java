769d68be6f626d7345e0c2b3c8884dba555a3dc3
Claudia Yarinet Osorio López <claus.yari.os@ciencias.unam.mx>
2024-03-11 00:40:02 -0600

    Template e Iteratos

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Lector lector = new Lector ();
-        IterableCollection<Hamburguesa> menu = new Delux();
-        IterableCollection<Hamburguesa> menu2 = new Basico();
-        IterableCollection<Hamburguesa> menu3 = new Especial();
-        lector.leeMenu(menu);
-        lector.leeMenu(menu2);
-        lector.leeMenu(menu3);
+        IterableCollection<Hamburguesa> menuDeluxe = new Delux();
+        IterableCollection<Hamburguesa> menuBasico = new Basico();
+        IterableCollection<Hamburguesa> menuEspecial = new Especial();
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        lector.leeMenu(menuDeluxe);
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        lector.leeMenu(menuBasico);
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        lector.leeMenu(menuEspecial);
+        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
     }
 }
